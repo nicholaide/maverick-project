d164d23fc3b9676b4109de9fe246402b129c0371
abhishek bhardwaj <abhishekbhardwaj007@gmail.com>
2012-01-20 17:07:48 -0600

    Added Message Passer Class and res folder with yaml course

---
+++ PeerApplication/src/MessagePasser.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class MessagePasser {
+
+	public MessagePasser(String configuration_filename, String local_name){
+		
+	}
+	
+	void send(Message message){
+		
+		
+	}
+	
+	Message receive(){
+		Message m = null;
+		return m;
+	}
+
+}
